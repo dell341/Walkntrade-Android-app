2f90bb02904364e531565b71b36095eaacf562b3
Nathan Louis <nlouis24@gmail.com>
2014-08-23 20:04:59 -0400

    Adjustments to full post image

--- src/com/walkntrade/ImageDialog.java
+++ src/com/walkntrade/ImageDialog.java
@@ -6,23 +6,30 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
-import com.walkntrade.asynctasks.ImageRetrievalTask;
 import com.walkntrade.io.DataParser;
+import com.walkntrade.io.DiskLruImageCache;
+
+import java.io.IOException;
 
 //Swipes through images of post
 public class ImageDialog extends Activity {
 
+    private static final String TAG = "ImageDialog";
     private static final String IMAGE_INDEX = "Index_of_image";
     private static final String IMAGE_URL = "Url_of_image";
-    private String TAG = "ImageDialog";
+
     private static Context context;
     private static String identifier;
     private String[] imgURLs;
@@ -77,18 +84,72 @@ public class ImageDialog extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_image, container, false);
 
-            ImageView image = (ImageView) rootView.findViewById(R.id.full_post_image);
+            ImageView imageView = (ImageView) rootView.findViewById(R.id.full_post_image);
+            ProgressBar progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
             Bundle args = getArguments();
             int index = args.getInt(IMAGE_INDEX);
             String url = args.getString(IMAGE_URL);
 
             if(DataParser.isNetworkAvailable(context)) {
                 //Calls single image to be displayed in pop-up
-                ImageRetrievalTask task = new ImageRetrievalTask(context, image, identifier, index);
+                ImageRetrievalTask task = new ImageRetrievalTask(index, imageView, progressBar);
                 task.execute(url);
             }
 
             return rootView;
+        }
+    }
+
+    private static class ImageRetrievalTask extends AsyncTask<String, Void, Bitmap>{
+
+        private int index;
+        private ImageView imageView;
+        private ProgressBar progressBar;
+
+        public ImageRetrievalTask(int index, ImageView imageView, ProgressBar progressBar){
+            this.index = index;
+            this.imageView = imageView;
+            this.progressBar = progressBar;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... imgURL) {
+            String schoolID = DataParser.getSharedStringPreference(context, DataParser.PREFS_SCHOOL, DataParser.S_PREF_SHORT);
+            DiskLruImageCache imageCache = new DiskLruImageCache(context, schoolID+DiskLruImageCache.IMAGE_DIRECTORY);
+
+            Bitmap bm = null;
+            try {
+
+                String key = identifier+"_"+index;
+
+                bm = imageCache.getBitmapFromDiskCache(key); //Try to retrieve image from Cache
+
+                if(bm == null) //If it doesn't exists, retrieve image from network
+                    bm = DataParser.loadBitmap(imgURL[0]);
+
+                imageCache.addBitmapToCache(key, bm); //Finally cache bitmap. Will override cache if already exists or write new cache
+            } catch (IOException e) {
+                Log.e(TAG, "Retrieving image", e);
+            }
+            finally{
+                imageCache.close();
+            }
+
+            return bm;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            progressBar.setVisibility(View.GONE);
+            if(bitmap != null) {
+                imageView.setVisibility(View.VISIBLE);
+                imageView.setImageBitmap(bitmap);
+            }
         }
     }
 
